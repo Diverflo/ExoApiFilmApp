78f052cbd96ee719010eeac58a839a84ed2e6dcf
Florent <miquel.florent@gmail.com>
2023-02-20 17:15:43 +0100

    statusbar update

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import MyNav from "./src/nav/BottomNav";
 import { store } from "./src/redux/store";
 import { Provider } from "react-redux";
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
     return (
