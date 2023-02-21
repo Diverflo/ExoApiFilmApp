3656995b6ab2e1afaa5a17c7d236a4118a8409a4
Florent <miquel.florent@gmail.com>
2023-02-21 11:29:13 +0100

    delete statusbar to resolve title under statusbar of android

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import MyNav from "./src/nav/BottomNav";
 import { store } from "./src/redux/store";
 import { Provider } from "react-redux";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
     return (
         <>
-            <StatusBar style="auto" />
             <Provider store={store}>
                 <NavigationContainer>
                     <MyNav />
